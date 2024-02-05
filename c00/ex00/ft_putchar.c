e7b87cca027aed9646c41a224735e059a144dbfd
pengyuguan1984 <pengyuguan1984@hotmail.com>
2024-02-05 11:54:22 +1030

    edit ex00

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
+void display_character(char c){
+	write(1,&c,1);
+}
+int main(){
+	char character = 'A';
+	display_character(character);
+	return 0;
 }
